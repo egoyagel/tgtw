87ade5fba45ed4c93cfa52653ce0424915ff5fc9
egoyagel <egoyagel@gmail.com>
2023-07-08 00:21:48 +0300

    Update SETTINGS.py

--- SETTINGS.py
+++ SETTINGS.py
@@ -1,12 +1,12 @@
-APP_KEY = 'XXXxxXXXXXxxXXXXxXxxxXxxXxXXxxXxXxXxXXXxX' 
-APP_SECRET = 'xxxXXXXXxXxXxxxXXxXxXxxXXXXxxXxXXXxxXXxxXxxxxxX' 
-OAUTH_TOKEN = 'XXxXXXXXxxXXxxXxXxxxXXXxXxxxXxXXXxxXxXxXxxXxxx'
-OAUTH_TOKEN_SECRET = 'XXXxXXXxXxxXXXxXxxXxXXxXXXxXxXXXXxXxXXxXX'
+APP_KEY = 'ia5p8tPLOJTWtRPShaeeJqDEX' 
+APP_SECRET = 'O7hysY2I5C7GnhkXRTvmd6pQPlXVIq7kZ2EkdZGWT6EtF6X6n2' 
+OAUTH_TOKEN = '1374330829321740288-48fl1v0ljDbcuHqGPDAdZDIlu95MAC'
+OAUTH_TOKEN_SECRET = '9JCbrWmHRgsCNLpKyt92mdfJ1LiQjNyAJrQalqO4ZjPrT'
 
 ## Your Telegram Channel Name ##
-channel_name = 'geremysays'
+channel_name = 'twtgtest'
 ## Telegram Access Token ##
-telegram_token = 'nnnnnnnnnnnnn:xxxXXXxXxXxXxXXxXxxXxxxxXx'
+telegram_token = '6362727038:AAFbWKiy9UEWdZYm1Qj0Y8yNo-1JbRdmlB8'
 ## Twitter User Name to get Timeline ##
-user_name = 'GeremySays'
+user_name = 'egoyagel'
 
